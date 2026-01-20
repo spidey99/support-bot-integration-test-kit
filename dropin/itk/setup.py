0a45ac22c499b4769410d15286952fd7332a38cc
spidey99 <spidey99@users.noreply.github.com>
2026-01-19 22:44:48 -0600

    Add setup.py shim for older pip versions

---
+++ dropin/itk/setup.py
@@ -0,0 +1,5 @@
+# Shim for older pip versions that don't support PEP 517 (pyproject.toml only).
+# If you see this, consider upgrading pip: python -m pip install --upgrade pip
+from setuptools import setup
+
+setup()
